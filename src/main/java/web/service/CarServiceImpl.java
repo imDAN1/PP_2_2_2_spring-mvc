dbf88e8368e918807ac37d62e95a2e0b0db6056d
Diman <dimanozdrin1999@gmail.com>
2023-11-16 19:43:35 +0300

    My solution

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -2,9 +2,7 @@ package web.service;
 
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CarServiceImpl implements CarService {
 
